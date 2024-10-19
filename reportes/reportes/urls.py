56571ab5d4b66d778b8736fd8b5494cffd385f3b
lauram354 <l.murciac@uniandes.edu.co>
2024-10-19 02:21:37 -0500

    ff

--- reportes/reportes/urls.py
+++ reportes/reportes/urls.py
@@ -21,8 +21,8 @@ from . import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home),
-    path('cronograma/', views.publicarPagos, name='publicarpagos'),
-    #path('', views.publicarPagos, name='publicarpagos'),
+    path('cronograma/', views.generar_lista_correos, name='generar_lista_correos'),
+    path('', views.generar_lista_correos, name='generar_lista_correos'),
     path('health/', v.health_check, name='health')
 
 ]
