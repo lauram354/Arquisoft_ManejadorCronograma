92935293768b8371ffad4c44778fe4a85b14a643
SamuelRamirezG <111009060+SamuelRamirezG@users.noreply.github.com>
2024-10-19 09:25:09 -0500

    jfjfsñ

--- reportes/reportes/urls.py
+++ reportes/reportes/urls.py
@@ -21,7 +21,6 @@ from . import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home),
-    path('cronograma/', views.generar_lista_correos, name='generar_lista_correos'),
     path('health/', v.health_check, name='health')
 
 ]
